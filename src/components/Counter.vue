80e73fa7a172a101a7b45d2fcaca3976cedaae3f
Adam Venecia <aveneci3@stu.southtexascollege.edu>
2025-02-17 23:35:23 -0600

    Added Counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const count = ref(0);
+const emit = defineEmits(['updateCount']);
+
+const increment = () => {
+  count.value++;
+  emit('updateCount', count.value);
+};
+</script>
+
+<template>
+  <div>
+    <button @click="increment">Increase Count</button>
+  </div>
+</template>
